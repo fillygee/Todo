0b2aafd253502f7e27678652b21f1792fd47733c
filomena <filomena.francisco@outlook.com>
2023-08-29 13:29:50 +0100

    added remove item by clicking checkbox functionality

--- todo/index.js
+++ todo/index.js
@@ -4,28 +4,39 @@ const addTodo = document.querySelector('#createtodobutton');
 const todoForm = document.querySelector('#createtodoform')
 
 
-
-
-
 function addTodos(e) {
 e.preventDefault();
-const taskText = todoInput.value
-const listItem = document.createElement("li");
-listItem.className = "todoitem"
-listItem.innerHTML = `
-<input type="checkbox" class="todocheckbox">
-                <p class="todo">
-                ${taskText}
-                </p>
-`
 
-todoContainer.appendChild(listItem);
+const taskText = todoInput.value
+if (!taskText) return; 
+
+const todoItem = document.createElement("div");
+
+const checkbox = document.createElement("input");
+checkbox.setAttribute("type", "checkbox")
+checkbox.className = "todocheckbox"
+
+checkbox.addEventListener("change", () => {
+    todoItem.remove();
+}  )
+
+
+
+todoItem.className = "todoitem";
+
+const taskTextNode = document.createElement("span");
+taskTextNode.textContent = taskText;
+
+todoItem.appendChild(checkbox);
+todoItem.appendChild(taskTextNode);
+
+todoContainer.appendChild(todoItem);
+
 todoInput = ''
+
 }
 
 todoForm.addEventListener("submit", addTodos);
 
-
-console.log(addTodo);
 
 
